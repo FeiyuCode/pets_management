10c41b945d6bf98a3e7a3d9505eb069779cf37ee
wongnoubo <1012084607@qq.com>
2018-11-02 21:55:58 +0800

    删除图片显示

--- USER/main.c
+++ USER/main.c
@@ -177,19 +177,7 @@ void readId(void)
 
 
  int main(void)
- {	 
-	u8 res;
- 	DIR picdir;	 		//图片目录
-	FILINFO picfileinfo;//文件信息
-	u8 *fn;   			//长文件名
-	u8 *pname;			//带路径的文件名
-	u16 totpicnum; 		//图片文件总数
-	u16 curindex;		//图片当前索引
-	u8 key;				//键值
-	u8 pause=0;			//暂停标记
-	u8 t;
-	u16 temp;
-	u16 *picindextbl;	//图片索引表 
+ {
 	int KeyValue=0;
 	 
 	delay_init();	    	 //延时函数初始化	  
@@ -199,11 +187,6 @@ void readId(void)
  	LED_Init();		  			//初始化与LED连接的硬件接口
 	KEY_Init();					//初始化按键
 	LCD_Init();			   		//初始化LCD   
-	W25QXX_Init();				//初始化W25Q128
- 	my_mem_init(SRAMIN);		//初始化内部内存池
-	exfuns_init();				//为fatfs相关变量申请内存  
- 	f_mount(fs[0],"0:",1); 		//挂载SD卡 
- 	f_mount(fs[1],"1:",1); 		//挂载FLASH.
 	KeyBoard_Init();//初始化矩阵键盘
 	Beep_Init();//初始化蜂鸣器
 	InitRc522();//初始化射频卡模块
@@ -232,75 +215,19 @@ while(HC05_Init()) 		//初始化ATK-HC05模块
 	Show_Str(50,70,200,16,"操作事项：",16,0);		
 	Show_Str(50,90,200,16,"KEY1阅读标签",16,0);			
 	Show_Str(50,110,200,16,"KEY2设置电话号码",16,0);		
-	Show_Str(50,130,200,16,"KEY3一键清零",16,0);		
- 	while(f_opendir(&picdir,"0:/PICTURE"))//打开图片文件夹
- 	{	    
-		Show_Str(30,170,240,16,"PICTURE文件夹错误!",16,0);
-		delay_ms(200);				  
-		LCD_Fill(30,170,240,186,WHITE);//清除显示	     
-		delay_ms(200);				  
-	}  
-	totpicnum=pic_get_tnum("0:/PICTURE"); //得到总有效文件数
-  	while(totpicnum==NULL)//图片文件为0		
- 	{	    
-		Show_Str(30,170,240,16,"没有图片文件!",16,0);
-		delay_ms(200);				  
-		LCD_Fill(30,170,240,186,WHITE);//清除显示	     
-		delay_ms(200);				  
-	}
-  	picfileinfo.lfsize=_MAX_LFN*2+1;						//长文件名最大长度
-	picfileinfo.lfname=mymalloc(SRAMIN,picfileinfo.lfsize);	//为长文件缓存区分配内存
- 	pname=mymalloc(SRAMIN,picfileinfo.lfsize);				//为带路径的文件名分配内存
- 	picindextbl=mymalloc(SRAMIN,2*totpicnum);				//申请2*totpicnum个字节的内存,用于存放图片索引
- 	while(picfileinfo.lfname==NULL||pname==NULL||picindextbl==NULL)//内存分配出错
- 	{	    
-		Show_Str(30,170,240,16,"内存分配失败!",16,0);
-		delay_ms(200);				  
-		LCD_Fill(30,170,240,186,WHITE);//清除显示	     
-		delay_ms(200);				  
-	}  	
-	//记录索引
-    res=f_opendir(&picdir,"0:/PICTURE"); //打开目录
-	if(res==FR_OK)
-	{
-		curindex=0;//当前索引为0
-		while(1)//全部查询一遍
-		{
-			temp=picdir.index;								//记录当前index
-	        res=f_readdir(&picdir,&picfileinfo);       		//读取目录下的一个文件
-	        if(res!=FR_OK||picfileinfo.fname[0]==0)break;	//错误了/到末尾了,退出		  
-     		fn=(u8*)(*picfileinfo.lfname?picfileinfo.lfname:picfileinfo.fname);			 
-			res=f_typetell(fn);	
-			if((res&0XF0)==0X50)//取高四位,看看是不是图片文件	
-			{
-				picindextbl[curindex]=temp;//记录索引
-				curindex++;
-			}	    
-		} 
-	}   
-	delay_ms(1500);
-	piclib_init();										//初始化画图	   	   
-	curindex=0;											//从0开始显示
-   	res=f_opendir(&picdir,(const TCHAR*)"0:/PICTURE"); 	//打开目录
-	while(res==FR_OK)//打开成功
-	{	
-		dir_sdi(&picdir,picindextbl[curindex]);			//改变当前目录索引	   
-        res=f_readdir(&picdir,&picfileinfo);       		//读取目录下的一个文件
-        if(res!=FR_OK||picfileinfo.fname[0]==0)break;	//错误了/到末尾了,退出
-     	fn=(u8*)(*picfileinfo.lfname?picfileinfo.lfname:picfileinfo.fname);			 
-		strcpy((char*)pname,"0:/PICTURE/");				//复制路径(目录)
-		strcat((char*)pname,(const char*)fn);  			//将文件名接在后面
- 		LCD_Clear(BLACK);
-		t=0;
+	Show_Str(50,130,200,16,"KEY3一键清零",16,0);	
+	delay_ms(1000);	
+	LCD_Clear(WHITE);
 		while(1) 
 		{
-			ai_load_picfile(pname,0,0,lcddev.width,lcddev.height,1);//显示背景图片
 			KeyValue = Keyscan();
 			switch(KeyValue){
 				case 1:
 				{
-					readId();
-					Show_Str(50,70,200,16,"操作事项1：",16,0);	
+					while(1){
+						readId();
+						Show_Str(50,70,200,16,"操作事项1：",16,0);	
+					}
 				}
 				break;
 				case 2:
@@ -310,12 +237,7 @@ while(HC05_Init()) 		//初始化ATK-HC05模块
 				break;
 			}
 		}					    
-		res=0;  
 	} 											  
-	myfree(SRAMIN,picfileinfo.lfname);	//释放内存			    
-	myfree(SRAMIN,pname);				//释放内存			    
-	myfree(SRAMIN,picindextbl);			//释放内存		
-}
 
 
 
